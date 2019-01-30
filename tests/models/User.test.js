b029acfff3c5ac6993aa709886f6e5ea0f531f24
Lance Owen Merrill <Lanceom@hawaii.edu>
2019-01-30 14:10:39 -0800

    fixed users

--- tests/models/User.test.js
+++ tests/models/User.test.js
@@ -1,20 +1,34 @@
 require('dotenv').config();
-require('../../lib/utils/connect');
+require('../../lib/utils/connect')();
 const mongoose = require('mongoose');
-// const { Types } = require('mongoose');
+const { Types } = require('mongoose');
 const User = require('../../lib/models/User');
 // const { tokenize, untokenize } = require('../../lib/utils/token');
 
 describe('User model', () => {
   beforeEach(done => {
-    mongoose.connection.dropDatabase(done);
+    return mongoose.connection.dropDatabase(done);
   });
+  afterAll(done => {
+    mongoose.connection.close(done);
+  });
+
 
   it('validates a good model', () => {
     const user = new User({
-      username: 'Bill'
+      username: 'Bill',
+      password: 'password', 
+      profilePhotoUrl: 'https://images-na.ssl-images-amazon.com/images/I/51ivptfSjlL.jpg'
     });
-    expect(user.toJSON()).toEqual({ username: 'Bill', id: expect.any(String) });
+    expect(user.toJSON()).toEqual({ username: 'Bill', _id: expect.any(Types.ObjectId), profilePhotoUrl: expect.any(String) });
+  });
+  it('stores a temp password', () => {
+    const user = new User({
+      username: 'Bill',
+      password: 'password', 
+      profilePhotoUrl: 'https://images-na.ssl-images-amazon.com/images/I/51ivptfSjlL.jpg'
+    });
+    expect(user._tempPassword).toEqual('password');
   });
 
   it('has a required passwordHash', () => {
@@ -26,21 +40,22 @@ describe('User model', () => {
       .then(user => {
         expect(user.passwordHash).toEqual(expect.any(String));
         expect(user.password).toBeUndefined();
-        expect(user.profilePhotoUrl).toEqual(expect.any(String));
       });
   });
 
-  // it('compares clear to hashedPassword', () => {
-  //   const user = new User({
-  //     username: 'Bill',
-  //     password: 'password',
-  //   });
-  //   return user.save()
-  //     .then(user => {
-  //       return user.compare('password');
-  //     })
-  //     .then(res => {
-  //       expect(res).toBeTruthy;
-  //     });
-  // });
+  it('compares clear to hashedPassword', () => {
+    const user = new User({
+      username: 'Bill',
+      password: 'password', 
+      profilePhotoUrl: 'https://images-na.ssl-images-amazon.com/images/I/51ivptfSjlL.jpg'
+    });
+    return user.save()
+      .then(user => {
+        return user.compare('password');
+      })
+      .then(res => {
+        expect(res).toBeTruthy;
+      });
+  });
+  
 });
