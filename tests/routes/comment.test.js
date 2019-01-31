11f40eb44fc05878b523399e778895a6a9035504
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-01-31 14:08:20 -0800

    1 comment route works

--- tests/routes/comment.test.js
+++ tests/routes/comment.test.js
@@ -1,14 +1,32 @@
-// require('dotenv').config();
-// require('../../lib/utils/connect')();
+require('dotenv').config();
+require('../../lib/utils/connect')();
 // const mongoose = require('mongoose');
-// const request = require('supertest');
-// const app = require('../../lib/app');
-// // const { getToken, getPost } = require('../dataHelper');
+const request = require('supertest');
+const app = require('../../lib/app');
+const { getToken, getPost } = require('../dataHelper');
 
-// describe('comment model', () => {
-//   afterAll(done => {
-//     mongoose.connection.close(done);
-//   });
 
-  
-// });
+describe('comment model', () => {
+
+  it('creates a comment', () => {
+    return getPost()
+      .then(post => {
+        return request(app)
+          .post('/comments')
+          .set('Authorization', `Bearer ${getToken()}`)
+          .send({
+            comment: 'hello',
+            post: post._id
+          })
+          .then(res => {
+            expect(res.body).toEqual({
+              comment: 'hello',
+              _id: expect.any(String),
+              commentBy: expect.any(String),
+              post: expect.any(String),
+              __v: 0
+            });
+          });
+      });
+  });
+});
