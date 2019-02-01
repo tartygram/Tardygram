5ca293ce9a1baba371124448695db7ac11b774da
Aaron Dennis <AMDennis1987@gmail.com>
2019-01-31 16:57:06 -0800

    added seedData object

--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ require('./lib/utils/connect')();
 const mongoose = require('mongoose');
 const seedData = require('./tests/seedData');
 
-seedData()
+seedData({})
   .then(() => {
     return console.log('****DONE****');
   })
